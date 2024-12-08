c7b8a4974fa1439c21dbb5256c28de415ea61fc3
sakur2023 <131665119+sakur2023@users.noreply.github.com>
2024-12-08 09:37:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ const require = createRequire(import.meta.url);
 
 const DATABASE_URL = process.env.DATABASE_URL || './database.db';
 const config = {
-	SESSION_ID: process.env.SESSION_ID || '',
-	PREFIX: process.env.PREFIX || '.',
-	SUDO: process.env.SUDO || '',
+	SESSION_ID: process.env.SESSION_ID || 'XSTRO_80_75_80',
+	PREFIX: process.env.PREFIX || ',',
+	SUDO: process.env.SUDO || '22998068954',
 	MENU_IMAGE_URL: 'https://avatars.githubusercontent.com/u/188756392?v=4', // add your images url if u want bot menu with image 
         MENU_DESIGN: 1, // 1 = Menu with image, 2 = Menu without image  pick style 1 if u want menu with image 
 	BOT_INFO: process.env.BOT_INFO || '𝙰𝚜𝚝𝚛𝚘;𝚇𝚜𝚝𝚛𝚘-𝙱𝚘𝚝',
